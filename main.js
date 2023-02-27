368b7e53d234d5d5d74be4008c41eaa667ddafc3
davoburgos <ldpinchao1@gmail.com>
2023-02-27 11:10:41 -0500

    vs1

---
+++ main.js
@@ -0,0 +1,40 @@
+// Estilos seleccionado
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    // desaparecemos el menu una vez seleccionado
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+// MENU R E S P O N S I V E
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
+
+// Detectar scrolling para aplicar la animación de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
