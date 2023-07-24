2ac7ab4c9dfe9af311d314df8bbf22dfe53d2c12
davidpinchao <ldpinchao1@gmail.com>
2023-07-24 16:26:56 -0500

    update index.html

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
+        document.getElementById("photoshop").classList.add("barra-progreso1");
+        document.getElementById("lightroom").classList.add("barra-progreso2");
+        document.getElementById("snapseed").classList.add("barra-progreso3");
+        document.getElementById("picsart").classList.add("barra-progreso4");
     }
 }
